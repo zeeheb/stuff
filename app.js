55e7b4dfe6f6fa33096576e0949ca37b8088a120
José Eduardo Brandão <39067212+zeeheb@users.noreply.github.com>
2020-01-21 17:02:03 -0300

    local storage done

---
+++ app.js
@@ -0,0 +1,61 @@
+const form = document.querySelector('.form');
+const submit = document.querySelector('.btn');
+
+loadEventListeners();
+
+function loadEventListeners() {
+  form.addEventListener('submit', escreveObj);
+}
+
+function escreveObj(e) {
+  const firstname = document.querySelector('.firstname');
+  const lastname = document.querySelector('.lastname');
+  const email = document.querySelector('.email');
+  const pw = document.querySelector('.pw');
+  const phone = document.querySelector('.phone');
+
+  let tempObj = {
+    fname: firstname.value,
+    lname: lastname.value,
+    em: email.value,
+    passw: pw.value,
+    phn: phone.value
+  };
+
+  let pessoasVec;
+
+  blankCheck = pessoasVec => {
+    if (
+      ['fname', 'lname', 'mail', 'pw', 'phone1'].some(function(item) {
+        return pessoasVec[item] === '';
+      })
+    ) {
+      alert('Preencha todos os campos!');
+      return false;
+    }
+    return true;
+  };
+
+  if (!blankCheck(tempObj)) {
+    return;
+  }
+
+  const dataFromStorage = localStorage.getItem('Contatos');
+
+  if (dataFromStorage) {
+    pessoasVec = JSON.parse(dataFromStorage);
+  } else {
+    pessoasVec = [];
+  }
+
+  pessoasVec.push(tempObj);
+
+  let objJson = JSON.stringify(pessoasVec);
+  localStorage.setItem('Contatos', objJson);
+
+  pessoasVec.forEach(pessoasVec => {
+    console.log(pessoasVec);
+  });
+
+  e.preventDefault();
+}
