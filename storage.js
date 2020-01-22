1aa841dc3f688c86bdaa0908f6b78b8bcc0a8243
José Eduardo Brandão <39067212+zeeheb@users.noreply.github.com>
2020-01-22 14:10:36 -0300

    storage fixed

--- storage.js
+++ storage.js
@@ -9,17 +9,25 @@ function loadContacts() {
   let pessoasVec = JSON.parse(data);
 
   const table = document.querySelector('.table');
-  const row = document.createElement('tr');
+  //   const row = document.createElement('tr');
 
   pessoasVec.forEach(pessoasVec => {
     console.log(pessoasVec);
-
+    const row = document.createElement('tr');
     const cell = document.createElement('td');
-    const celltext = document.createTextNode(pessoasVec.fname + ' ');
-    const celltext2 = document.createTextNode(pessoasVec.lname);
+    const cellfname = document.createTextNode(pessoasVec.fname + ' ');
+    const celllname = document.createTextNode(pessoasVec.lname + ' ');
+    const cellemail = document.createTextNode(pessoasVec.em + ' ');
+    //const cellpw = document.createTextNode(pessoasVec.passw + ' ');
+    const cellphone = document.createTextNode(pessoasVec.phn + ' ');
 
-    cell.appendChild(celltext);
-    cell.appendChild(celltext2);
+    const mybr = document.createElement('br');
+
+    cell.appendChild(cellfname);
+    cell.appendChild(celllname);
+    cell.appendChild(cellemail);
+    //cell.appendChild(cellpw);
+    cell.appendChild(cellphone);
     row.appendChild(cell);
 
     table.appendChild(row);
